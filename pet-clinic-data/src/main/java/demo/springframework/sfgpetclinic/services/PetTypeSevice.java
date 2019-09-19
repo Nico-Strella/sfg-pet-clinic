95a911982dae032fe5cad7e6e6e140d872dfed0b
nstrella <v-nstrella@expedia.com>
2019-09-19 17:09:52 -0300

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/demo/springframework/sfgpetclinic/services/PetTypeSevice.java
@@ -0,0 +1,6 @@
+package demo.springframework.sfgpetclinic.services;
+
+import demo.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeSevice extends CrudService<PetType, Long> {
+}
