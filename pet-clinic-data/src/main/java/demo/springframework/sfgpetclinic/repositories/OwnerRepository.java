b0d6044523cf98d6bdbcb53e1ce18e64cf830191
nstrella <v-nstrella@expedia.com>
2019-09-23 17:42:08 -0300

    Added spring data jpa service for owners. Closes #40

--- pet-clinic-data/src/main/java/demo/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/demo/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import demo.springframework.sfgpetclinic.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
